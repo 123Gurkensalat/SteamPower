fa1166e90cd64c592f2697f067ad055af1847022
Florian Zinck <florianzinck361@gmail.com>
2025-07-30 00:50:52 +0200

    chore: added helper to copy to vs mod folder

--- build.sh
+++ build.sh
@@ -1,1 +1,26 @@
 dotnet run --project ./CakeBuild/CakeBuild.csproj -- "$@"
+
+# copy .zip file to modfolder if set
+if [[ ! -f .env ]]; then
+    exit 1
+fi
+
+set -o allexport
+source .env
+set +o allexport
+
+if [[ -z "VINTAGE_STORY_MOD_FOLDER" ]]; then
+  echo "VINTAGE_STORY_MOD_FOLDER not set"
+  exit 1
+fi
+
+ZIP_FILE=$(ls -t ./Releases/*.zip 2>/dev/null | head -n 1)
+
+if [[ -z "$ZIP_FILE" ]]; then
+  echo ".zip-File not found"
+  exit 1
+fi
+
+cp "$ZIP_FILE" "$VINTAGE_STORY_MOD_FOLDER"
+
+echo "copied $ZIP_FILE"

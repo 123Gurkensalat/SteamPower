e558ba879555442242c4fd181ac7fc11a0d300b5
Florian Zinck <florianzinck361@gmail.com>
2025-08-03 02:00:56 +0200

    idea of steampot temperature

--- SteamPower/Blocks/BlockSteampot.cs
+++ SteamPower/Blocks/BlockSteampot.cs
@@ -4,20 +4,24 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 using Vintagestory.API.MathTools;
-
+using Vintagestory.GameContent;
 
 namespace SteamPower.Blocks
 {
-
     internal class BlockSteampot : Block
     {
+        BlockEntityFirepit firepit;
+        float temperature;
+        public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibPos)
+        {
+            base.OnNeighbourBlockChange(world, pos, neibPos);
+            if (neibPos.Y >= pos.Y) return;
+            firepit = GetBlockEntity<BlockEntityFirepit>(neibPos);
+            api.World.RegisterGameTickListener(Update, 500);
+        }
 
-
-
-        //Any code within this 'override' function will be called when a trampoline block is placed. 
         public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
         {
             api.Logger.Chat("Steam Pot Block Placed!");
@@ -25,6 +29,12 @@ namespace SteamPower.Blocks
             api.Logger.Notification("Steam Pot Block Placed!");
             //Perform any default logic when our block is placed.
             base.OnBlockPlaced(world, blockPos, byItemStack);
+        }
+
+        private void Update(float dTime)
+        {
+            // I think this is a logistic function
+            temperature += dTime + dTime * System.Math.Abs(temperature - firepit.furnaceTemperature);
         }
     }
 }
